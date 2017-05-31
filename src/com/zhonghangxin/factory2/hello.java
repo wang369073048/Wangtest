8e79bb07349386b39909f6afee9e219138a8e710
wangzhen369 <369073048@qq.com>
2017-05-31 15:05:58 +0800

    update

---
+++ src/com/zhonghangxin/factory2/hello.java
@@ -0,0 +1,18 @@
+package com.zhonghangxin.factory2;
+
+
+//interface fruit{
+//	public abstract eat();
+//}
+ class hello {
+
+	public static void main(String[] args) {
+            
+		System.out.println("this is commit test !!");
+		System.out.println("this is branch test !!");
+		System.out.println("this is !!!!!!");
+		System.out.println("this is !!!!!!");
+		System.out.println("sdfsdfs   sdfsdf");
+	}
+
+}
